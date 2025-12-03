20067763d03c5dda8f8c2ca7b0c92f322a9fa7b1
Lee Sheppard <lee@leesheppard.com>
2025-12-03 23:01:25 +1100

    Create start.sh

---
+++ bin/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Prepare database (create tables if needed)
+bundle exec rake db:prepare
+
+# Start Sidekiq worker in the background
+bundle exec sidekiq -r ./app.rb -c 5 -e ${RACK_ENV:-production} &
+SIDEKIQ_PID=$!
+
+# Start the web server
+bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-production}
+
+# Keep Sidekiq running if the web server exits
+wait $SIDEKIQ_PID
