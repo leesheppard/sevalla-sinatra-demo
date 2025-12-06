cc6e663e66690873130e4925a28235bedfceebd0
Lee Sheppard <lee@leesheppard.com>
2025-12-06 23:09:15 +1100

    Refactor database connection setup
    Simplified database configuration to use DATABASE_URL if available, falling back to default settings otherwise. Removed inline table creation logic for users and orders.

--- config/database.rb
+++ config/database.rb
@@ -1,29 +1,15 @@
 require 'active_record'
 
-db_config = {
-  adapter: 'postgresql',
-  database: ENV['DATABASE_URL'] || 'sinatra_exports_dev',
-  user: ENV['DATABASE_USER'] || '',
-  password: ENV['DATABASE_PASSWORD'] || '',
-  host: ENV['DATABASE_HOST'] || 'localhost',
-  port: ENV['DATABASE_PORT'] || 5432
-}
-
-ActiveRecord::Base.establish_connection(db_config)
-
-unless ActiveRecord::Base.connection.table_exists?(:users)
-  ActiveRecord::Base.connection.create_table(:users) do |t|
-    t.string :name
-    t.string :email
-    t.timestamps
-  end
-end
-
-unless ActiveRecord::Base.connection.table_exists?(:orders)
-  ActiveRecord::Base.connection.create_table(:orders) do |t|
-    t.integer :user_id
-    t.decimal :amount
-    t.string :status
-    t.timestamps
-  end
+if ENV['DATABASE_URL']
+  ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
+else
+  db_config = {
+    adapter: 'postgresql',
+    database: 'sinatra_exports_dev',
+    user: ENV['DATABASE_USER'] || '',
+    password: ENV['DATABASE_PASSWORD'] || '',
+    host: ENV['DATABASE_HOST'] || 'localhost',
+    port: ENV['DATABASE_PORT'] || 5432
+  }
+  ActiveRecord::Base.establish_connection(db_config)
 end
