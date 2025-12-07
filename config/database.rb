bf5750115ca30690d0fa508a2e0dc1b9dca874ca
Lee Sheppard <lee@leesheppard.com>
2025-12-07 15:34:17 +1100

    Update database config defaults and add connection log
    Ensured port is an integer. Added a log message to confirm database connection details.

--- config/database.rb
+++ config/database.rb
@@ -6,10 +6,12 @@ else
   db_config = {
     adapter: 'postgresql',
     database: ENV['DATABASE'] || 'sinatra_exports_dev',
-    user: ENV['DATABASE_USER'] || '',
-    password: ENV['DATABASE_PASSWORD'] || '',
+    username: ENV['DATABASE_USER'] || 'postgres',
+    password: ENV['DATABASE_PASSWORD'] || nil,
     host: ENV['DATABASE_HOST'] || 'localhost',
-    port: ENV['DATABASE_PORT'] || 5432
+    port: (ENV['DATABASE_PORT'] || 5432).to_i
   }
+
   ActiveRecord::Base.establish_connection(db_config)
+  puts "Connected to #{ActiveRecord::Base.connection_config[:database]} on #{ActiveRecord::Base.connection_config[:host]}"
 end
